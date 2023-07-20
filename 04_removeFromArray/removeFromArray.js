ab88950f5591b697459f3f1334ee04f0b45b868d
Louism <8287251+louisfm16@users.noreply.github.com>
2023-07-19 18:06:17 -0700

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let newArr = arr;
 
+    for (let i = 0; i< args.length; i++) { // Loop through all the arguments\
+        newArr = [...newArr.filter(function(itm) {
+            return itm !== args[i];
+        })]
+    }
+
+    return newArr;
 };
 
 // Do not edit below this line
