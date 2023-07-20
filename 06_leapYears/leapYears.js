d4c8b97feb558c9a1aa866c72ce5337ce24fbc3b
Louism <8287251+louisfm16@users.noreply.github.com>
2023-07-19 18:51:38 -0700

    Completed exercises 5-7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeap = false;
+    // If Divisible by 4 = true
+    if(year % 4 == 0) {
+        isLeap = true;
+        if(year % 100 == 0 && year % 400 != 0) {
+            isLeap = false;
+        }
+    }   
 
+    return isLeap;
 };
 
 // Do not edit below this line
