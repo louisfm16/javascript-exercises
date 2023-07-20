f8261cc9ab6d9162b57d13b7f0831bc4b669b3dd
Louism <8287251+louisfm16@users.noreply.github.com>
2023-07-19 17:25:59 -0700

    Completed exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    if(n<0) return 'ERROR';
+    
+    let fullString = '';
 
+    for(let i = 0; i<n; i++) {
+        fullString += str;
+    }
+
+    return fullString;
 };
 
 // Do not edit below this line
