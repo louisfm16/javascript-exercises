81cea38b7110fef2b4e82e8ae4fa9d4be2fd075a
Louism <8287251+louisfm16@users.noreply.github.com>
2023-07-31 19:55:23 -0700

    Completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+
+  for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(...args) {
+  let multSum = 1;
 
+  for(let i = 0; i < args.length; i++) {
+    multSum *= args[i];
+  }
+
+  return multSum;
 };
 
-const power = function() {
-	
+const power = function(n, pwr) {
+	let num = 1;
+
+  for (let i = 0; i < pwr; i++) {
+    num *= n;
+  }
+
+  return num;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
