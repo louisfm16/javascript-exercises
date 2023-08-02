ea919c4e246458dfe9a8b3355c6f408e97627d8e
Louism <8287251+louisfm16@users.noreply.github.com>
2023-08-01 21:23:16 -0700

    Completed excercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestP, currP) => findPersonAge(currP) > findPersonAge(oldestP) ? currP : oldestP);
 };
+
+const findPersonAge = function(person) {
+    if(!person.hasOwnProperty('yearOfDeath'))
+        person.yearOfDeath = (new Date().getFullYear());
+
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
