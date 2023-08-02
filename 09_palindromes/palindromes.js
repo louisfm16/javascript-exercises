3ccc579ef76716ef6c4bb6649190f758024a0825
Louism <8287251+louisfm16@users.noreply.github.com>
2023-08-01 20:07:52 -0700

    Completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let myStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return myStr.split("").reverse().join("") == myStr;
 };
 
 // Do not edit below this line
