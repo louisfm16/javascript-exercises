eb132b83b4e85c81faddcb74b76b8e66d4cf99e0
Louism <8287251+louisfm16@users.noreply.github.com>
2023-08-01 20:49:41 -0700

    Completed excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(nFib) {
+    nFib = parseInt(nFib);
+    if(nFib <= 0) return "OOPS";
+    if(nFib <= 2) return 1;
 
+    let myFibs = [1, 1];
+
+    for(let i = 2; i < nFib; i++) {
+        last2Sum = myFibs[i-2] + myFibs[i-1];
+        myFibs.push(last2Sum);
+    }
+
+    return myFibs[myFibs.length-1];
 };
 
 // Do not edit below this line
